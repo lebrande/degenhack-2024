0334096ae15b01b0e9bd67ceaf8f2e9c4c925049
JS Dzem <jsdzem@gmail.com>
2024-02-16 14:08:16 +0100

    Flash loan contract scaffolding

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,5 +1,12 @@
+import hre from "hardhat";
+
+// Ethereum
+const PoolAddressesProvider_Aave = '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e';
+
 async function main() {
-  
+  const fixedStakeRate = await hre.viem.deployContract("FixedStakeRate", [PoolAddressesProvider_Aave]);
+
+  console.log(`FixedStakeRate contract deployed: ${fixedStakeRate.address}`);
 }
 
 main().catch((error) => {
